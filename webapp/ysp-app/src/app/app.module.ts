f6132fd52f43fb15ac0cf095470f6bf9bc5eb566
Aliaksandr Yushkevich <umnix!@ya.ru>
2017-06-29 22:16:05 +0300

    [pB-005] webapp: 1. Fixed dependencies' versions, 2. @angular/material is ready to use, 3. deleted unnecessary test-files(spec.ts) 4. upgraded app structure

--- webapp/ysp-app/src/app/app.module.ts
+++ webapp/ysp-app/src/app/app.module.ts
@@ -1,24 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from "@angular/material";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PagesModule } from "app/pages/pages.module";
+import { ServicesModule } from "app/services/services.module";
+import { SharedModule } from "app/shared/shared.module";
 
 import { AppComponent } from './app.component';
-import { MusicPuzzleComponent } from './pages/music-puzzle/music-puzzle.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MusicPuzzleComponent
+    AppComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    PagesModule, 
+    ServicesModule,
+    SharedModule,
+    MaterialModule,
   ],
-  providers: [],
+  exports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    PagesModule, 
+    ServicesModule,
+    SharedModule,
+    MaterialModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ webapp/ysp-app/src/app/pages/pages.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MusicPuzzleComponent } from "app/pages/music-puzzle/music-puzzle.component";
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        MusicPuzzleComponent
+    ]
+})
+export class PagesModule { }
